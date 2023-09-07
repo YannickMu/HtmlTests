78f0e1ed6876753eb69bacb13d12dc343ac23f84
Trompeterli123 <117479959+Trompeterli123@users.noreply.github.com>
2023-09-07 20:49:27 +0200

    first upload

---
+++ GlowingSchriftAnimiert/Jaël.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+filename = "C:/Users/YANNI/Desktop/Informatik/HTMLusw/TestFürWebsiteNeu/GlowingSchriftAnimiert/Jaël.html"
+
+webbrowser.open_new_tab("file://" + filename)
---
+++ GlowingSchriftAnimiert/Mia.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+filename = "C:/Users/YANNI/Desktop/Informatik/HTMLusw/TestFürWebsiteNeu/GlowingSchriftAnimiert/Mia.html"
+
+webbrowser.open_new_tab("file://" + filename)
